2c20fb13ae26759511bfc75cd0468e8fbda847f1
fmidyt <fmidyt@gmail.com>
2022-02-09 16:26:11 +0700

    feat(types): register form struct

--- src/types/user.go
+++ src/types/user.go
@@ -1,6 +1,15 @@
 package types
 
+import "time"
+
 type LoginForm struct {
 	Email    string `form:"email" json:"email" binding:"required,email"`
 	Password string `form:"password" json:"password" binding:"required,min=3,max=50"`
+}
+
+type RegisterForm struct {
+	FirstName   string    `form:"firstName" json:"firstName" binding:"required,min=3,max=50"`
+	LastName    string    `form:"lastName" json:"lastName" binding:"required,min=3,max=50"`
+	PhoneNumber string    `form:"phoneNumber" json:"phoneNumber" binding:"required,min=10,max=15"`
+	DateOfBirth time.Time `form:"dateOfBirth" json:"dateOfBirth" binding:"required"`
 }

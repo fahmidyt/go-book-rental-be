d30fdb6a7173ef8f38bc432ef057a46ba4ec5b75
fmidyt <fmidyt@gmail.com>
2022-02-11 10:42:55 +0700

    refactor(db): declare gorm.DB & GetDB()

--- src/db/db.go
+++ src/db/db.go
@@ -9,15 +9,20 @@ import (
 	"gorm.io/gorm"
 )
 
-func OpenConnection() *gorm.DB {
+var db *gorm.DB
+
+func OpenConnection() {
+	var err error
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	// throw error if error
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
+func GetDB() *gorm.DB {
 	return db
 }

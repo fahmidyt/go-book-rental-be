b2972f84930639f08e465a9d1afe882719120d00
fmidyt <fmidyt@gmail.com>
2022-02-11 10:45:49 +0700

    feat(controller): add login and register auth

--- src/controllers/auth.go
+++ src/controllers/auth.go
@@ -3,16 +3,17 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/fahmidyt/go-book-rental-be/src/models"
+	"github.com/fahmidyt/go-book-rental-be/src/services"
 	"github.com/fahmidyt/go-book-rental-be/src/types"
 	"github.com/gin-gonic/gin"
 )
 
 type AuthController struct{}
 
-var userModel = new(models.User)
+// declare auth controller
+var authService = new(services.AuthService)
 
-func (ctl AuthController) Login(c *gin.Context) {
+func (ctrl AuthController) Login(c *gin.Context) {
 	var body types.LoginForm
 
 	// if validation error
@@ -22,4 +23,45 @@ func (ctl AuthController) Login(c *gin.Context) {
 		return
 	}
 
+	user, token, err := authService.Login(body)
+
+	if err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{
+			"message": message,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully login!",
+		"user":    user,
+		"token":   token,
+	})
+}
+
+func (ctrl AuthController) Register(c *gin.Context) {
+	var body types.RegisterForm
+
+	// if validation error
+	if validationErr := c.ShouldBindJSON(&body); validationErr != nil {
+		message := "Your data is invalid. Please check your information again"
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"message": message})
+		return
+	}
+
+	user, err := authService.Register(body)
+
+	if err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{
+			"message": message,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully register",
+		"data":    user,
+	})
 }

2c2c18bef51e382ff504090a2da9476f4a1002d2
fmidyt <fmidyt@gmail.com>
2022-02-11 10:44:32 +0700

    feat(helper): UnmarshalJSON of Time

---
+++ src/helpers/time.go
@@ -0,0 +1,19 @@
+package helpers
+
+import "time"
+
+type Time struct {
+	time.Time
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" || string(data) == `""` {
+		return nil
+	}
+
+	// Fractional seconds are handled implicitly by Parse.
+	tt, err := time.Parse(`"`+time.RFC3339+`"`, string(data))
+	*t = Time{tt}
+	return err
+}

91c5bf8904f3db80bd4690069bf799f0c3f3351b
fmidyt <fmidyt@gmail.com>
2022-02-12 03:00:27 +0700

    feat(route): v1 route

--- src/routes/v1routes.go
+++ src/routes/v1routes.go
@@ -1,7 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fahmidyt/go-book-rental-be/src/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var bookController = new(controllers.BookController)
 
 func V1Routes(r *gin.RouterGroup) {
-
+	r.GET("/book", bookController.GetAll)
 }

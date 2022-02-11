90d5ac18938f3b242ac643272b281b93d881e829
fmidyt <fmidyt@gmail.com>
2022-02-11 10:44:53 +0700

    refactor(type): user form

--- src/types/user.go
+++ src/types/user.go
@@ -1,6 +1,6 @@
 package types
 
-import "time"
+import "github.com/fahmidyt/go-book-rental-be/src/helpers"
 
 type LoginForm struct {
 	Email    string `form:"email" json:"email" binding:"required,email"`
@@ -8,8 +8,10 @@ type LoginForm struct {
 }
 
 type RegisterForm struct {
-	FirstName   string    `form:"firstName" json:"firstName" binding:"required,min=3,max=50"`
-	LastName    string    `form:"lastName" json:"lastName" binding:"required,min=3,max=50"`
-	PhoneNumber string    `form:"phoneNumber" json:"phoneNumber" binding:"required,min=10,max=15"`
-	DateOfBirth time.Time `form:"dateOfBirth" json:"dateOfBirth" binding:"required"`
+	LoginForm
+	ConfirmPassword string       `form:"confirmPassword" json:"confirmPassword" binding:"required,eqfield=Password"`
+	FirstName       string       `form:"firstName" json:"firstName" binding:"required,min=3,max=50,min=3,max=50"`
+	LastName        string       `form:"lastName" json:"lastName" binding:"required,min=3,max=50"`
+	PhoneNumber     string       `form:"phoneNumber" json:"phoneNumber" binding:"required,min=10,max=15"`
+	DateOfBirth     helpers.Time `form:"dateOfBirth" json:"dateOfBirth" binding:"required"`
 }

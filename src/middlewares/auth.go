ea38d8d3e72dbf23a6c08d0640a2a8a74b131541
fmidyt <fmidyt@gmail.com>
2022-02-18 03:48:25 +0700

    feat(middlewares): AuthMiddleware verify token

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -1,8 +1,15 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fahmidyt/go-book-rental-be/src/helpers"
+	"github.com/gin-gonic/gin"
+)
 
 func AuthMiddleware() gin.HandlerFunc {
+	return helpers.VerifyAccesToken
+}
+
+func AdminAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// TODO: add auth token validation
 		c.Next()

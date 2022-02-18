84b43d27288a022d6ff4ce070edf459538a2842e
fmidyt <fmidyt@gmail.com>
2022-02-18 10:53:44 +0700

    feat: user routes

--- src/routes/v1routes.go
+++ src/routes/v1routes.go
@@ -2,11 +2,20 @@ package routes
 
 import (
 	"github.com/fahmidyt/go-book-rental-be/src/controllers"
+	"github.com/fahmidyt/go-book-rental-be/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 var bookController = new(controllers.BookController)
+var userController = new(controllers.UserController)
 
 func V1Routes(r *gin.RouterGroup) {
 	r.GET("/book", bookController.GetAll)
+
+	adminRoute := r.Group("/admin")
+	{
+		adminRoute.Use(middlewares.AdminAuthMiddleware())
+
+		adminRoute.GET("/user", userController.GetAll)
+	}
 }

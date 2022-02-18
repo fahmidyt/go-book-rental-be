c91ff3193dcda043d6a6a27ba5b78060917c3855
fmidyt <fmidyt@gmail.com>
2022-02-18 12:32:01 +0700

    feat(middlewares): AdminAuthMiddleware

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -1,7 +1,11 @@
 package middlewares
 
 import (
+	"net/http"
+
+	"github.com/fahmidyt/go-book-rental-be/src/db"
 	"github.com/fahmidyt/go-book-rental-be/src/helpers"
+	"github.com/fahmidyt/go-book-rental-be/src/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,7 +15,25 @@ func AuthMiddleware() gin.HandlerFunc {
 
 func AdminAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// TODO: add auth token validation
+		user := c.MustGet("User").(map[string]interface{})
+
+		result := new(models.Role)
+		res := db.GetDB().Model(&result).First(result, models.Role{Name: "Admin"})
+
+		if res.Error != nil {
+			c.AbortWithStatusJSON(http.StatusConflict, gin.H{
+				"message": "Role Admin cannot be found in database!",
+			})
+			return
+		}
+
+		if uint(user["RoleId"].(float64)) != result.ID {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
+			})
+			return
+		}
+
 		c.Next()
 	}
 }
